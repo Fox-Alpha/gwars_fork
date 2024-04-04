78bda6f9443a0d1e350ea0412b8162bb42f249cc
Fox-Alpha <fox-alpha@tarnhoerner.de>
2024-04-04 15:39:14 +0200

    Upd: Autoupdate Spacing / Tabs Add: Progressbar for visualisation of gathering progress

--- scenes/units/minions/Worker/Worker.cs
+++ scenes/units/minions/Worker/Worker.cs
@@ -4,147 +4,165 @@ using System;
 
 public partial class Worker : Unit
 {
-    public float Speed = 300.0f;
-    public uint MaxMaterialsInBag { get; set; } = 5;
-    public float GatherSpeed = 1.0f;
-    public uint GatherEfficiency = 1;
-    private Area2D interactArea = null;
-    private Timer gatherTimer = null;
+	public float Speed = 300.0f;
+	public uint MaxMaterialsInBag { get; set; } = 5;
+	public float GatherSpeed = 1.0f;
+	public uint GatherEfficiency = 1;
+	private Area2D interactArea = null;
+	private Timer gatherTimer = null;
+	private ProgressBar progressBar = null;
 
-    private Material materialTarget = null;
-    private Unit storageTarget = null;
+	private Material materialTarget = null;
+	private Unit storageTarget = null;
 
-    private Array<Node> bodiesInInteractRange = new Array<Node>();
+	private Array<Node> bodiesInInteractRange = new Array<Node>();
 
-    [Export]
-    private string materialTypeInBag = "";
-    [Export]
-    private uint amountOfMaterialInBag = 0;
-
-
-    public override void _Ready()
-    {
-        base._Ready();
-
-        interactArea = GetNode<Area2D>("%InteractArea");
-        interactArea.BodyEntered += OnInteractAreaBodyEntered;
-        interactArea.BodyExited += OnInteractAreaBodyExited;
-
-        gatherTimer = new Timer();
-        gatherTimer.Name = "GatherTimer";
-        gatherTimer.OneShot = true;
-        gatherTimer.WaitTime = 1.0f;
-        gatherTimer.Autostart = false;
-        gatherTimer.Timeout += OnGatherTimerTimeout;
-        AddChild(gatherTimer);
-    }
+	[Export]
+	private string materialTypeInBag = "";
+	[Export]
+	private uint amountOfMaterialInBag = 0;
+	public uint AmountOfMaterialInBag
+	{
+		get { return amountOfMaterialInBag; }
+		set
+		{
+			amountOfMaterialInBag = value;
+			progressBar.Value = value;
+		}
+	}
 
 
-    public override void _PhysicsProcess(double delta)
-    {
-        if (materialTarget != null && amountOfMaterialInBag < MaxMaterialsInBag)
-        {
-            if (!bodiesInInteractRange.Contains(materialTarget))
-            {
-                Velocity = Position.DirectionTo(materialTarget.Position) * Speed;
-            }
-            else
-            {
-                Velocity = Vector2.Zero;
+	public override void _Ready()
+	{
+		base._Ready();
 
-                if (gatherTimer.IsStopped())
-                {
-                    gatherTimer.Start(GatherSpeed);
-                }
-            }
+		interactArea = GetNode<Area2D>("%InteractArea");
+		interactArea.BodyEntered += OnInteractAreaBodyEntered;
+		interactArea.BodyExited += OnInteractAreaBodyExited;
 
-        }
-        else if (storageTarget != null)
-        {
-            if (!bodiesInInteractRange.Contains(storageTarget))
-            {
-                Velocity = Position.DirectionTo(storageTarget.Position) * Speed;
-            }
-            else
-            {
-                Velocity = Vector2.Zero;
+		progressBar = GetNode<ProgressBar>("%ProgressBar");
+		progressBar.MaxValue = MaxMaterialsInBag;
 
-                if (storageTarget.StoreMaterial(materialTypeInBag, amountOfMaterialInBag))
-                {
-                    amountOfMaterialInBag = 0;
-                    materialTypeInBag = "";
-                }
-            }
-        }
-        else if (Position.DistanceTo(targetPosition) > 8.0f)
-        {
-            Velocity = Position.DirectionTo(targetPosition) * Speed;
+		AmountOfMaterialInBag = 0;
 
-        }
-        else
-        {
-            Velocity = Vector2.Zero;
-        }
+		gatherTimer = new Timer();
+		gatherTimer.Name = "GatherTimer";
+		gatherTimer.OneShot = true;
+		gatherTimer.WaitTime = 1.0f;
+		gatherTimer.Autostart = false;
+		gatherTimer.Timeout += OnGatherTimerTimeout;
+		AddChild(gatherTimer);
+	}
 
-        MoveAndSlide();
-    }
 
-    public override void MoveTo(Vector2 position)
-    {
-        targetPosition = position;
-        materialTarget = null;
-        storageTarget = null;
-    }
+	public override void _PhysicsProcess(double delta)
+	{
+		if (materialTarget != null && AmountOfMaterialInBag < MaxMaterialsInBag)
+		{
+			if (!bodiesInInteractRange.Contains(materialTarget))
+			{
+				Velocity = Position.DirectionTo(materialTarget.Position) * Speed;
+			}
+			else
+			{
+				Velocity = Vector2.Zero;
 
-    public override void GatherMaterial(Material material, Unit Storage)
-    {
-        GD.Print("Gathering material from " + material.Name + " to " + Storage.Name);
-        targetPosition = material.Position;
-        materialTarget = material;
-        storageTarget = Storage;
-    }
+				if (gatherTimer.IsStopped())
+				{
+					gatherTimer.Start(GatherSpeed);
+				}
+			}
 
-    private void OnInteractAreaBodyEntered(Node body)
-    {
-        if (!bodiesInInteractRange.Contains(body))
-        {
-            bodiesInInteractRange.Add(body);
-        }
-    }
+		}
+		else if (storageTarget != null)
+		{
+			if (!bodiesInInteractRange.Contains(storageTarget))
+			{
+				Velocity = Position.DirectionTo(storageTarget.Position) * Speed;
+			}
+			else
+			{
+				Velocity = Vector2.Zero;
 
-    private void OnInteractAreaBodyExited(Node body)
-    {
-        if (bodiesInInteractRange.Contains(body))
-        {
-            bodiesInInteractRange.Remove(body);
-        }
-    }
+				if (storageTarget.StoreMaterial(materialTypeInBag, AmountOfMaterialInBag))
+				{
+					AmountOfMaterialInBag = 0;
+					//progressBar.Value = AmountOfMaterialInBag;
+					materialTypeInBag = "";
+				}
+			}
+		}
+		else if (Position.DistanceTo(targetPosition) > 8.0f)
+		{
+			Velocity = Position.DirectionTo(targetPosition) * Speed;
 
-    private void OnGatherTimerTimeout()
-    {
-        if (materialTarget == null)
-        {
-            return;
-        }
+		}
+		else
+		{
+			Velocity = Vector2.Zero;
+		}
 
-        if (!bodiesInInteractRange.Contains(materialTarget))
-        {
-            return;
-        }
+		MoveAndSlide();
+	}
 
-        if (amountOfMaterialInBag >= MaxMaterialsInBag)
-        {
-            return;
-        }
+	public override void MoveTo(Vector2 position)
+	{
+		targetPosition = position;
+		materialTarget = null;
+		storageTarget = null;
+	}
 
-        if (materialTypeInBag == materialTarget.MaterialType)
-        {
-            amountOfMaterialInBag = Math.Min(MaxMaterialsInBag, amountOfMaterialInBag + GatherEfficiency);
-        }
-        else
-        {
-            materialTypeInBag = materialTarget.MaterialType;
-            amountOfMaterialInBag = GatherEfficiency;
-        }
-    }
+	public override void GatherMaterial(Material material, Unit Storage)
+	{
+		GD.Print("Gathering material from " + material.Name + " to " + Storage.Name);
+		targetPosition = material.Position;
+		materialTarget = material;
+		storageTarget = Storage;
+	}
+
+	private void OnInteractAreaBodyEntered(Node body)
+	{
+		if (!bodiesInInteractRange.Contains(body))
+		{
+			bodiesInInteractRange.Add(body);
+		}
+	}
+
+	private void OnInteractAreaBodyExited(Node body)
+	{
+		if (bodiesInInteractRange.Contains(body))
+		{
+			bodiesInInteractRange.Remove(body);
+		}
+	}
+
+	private void OnGatherTimerTimeout()
+	{
+		if (materialTarget == null)
+		{
+			return;
+		}
+
+		if (!bodiesInInteractRange.Contains(materialTarget))
+		{
+			return;
+		}
+
+		if (AmountOfMaterialInBag >= MaxMaterialsInBag)
+		{
+			return;
+		}
+
+		if (materialTypeInBag == materialTarget.MaterialType)
+		{
+			AmountOfMaterialInBag = Math.Min(MaxMaterialsInBag, AmountOfMaterialInBag + GatherEfficiency);
+			//progressBar.Value = AmountOfMaterialInBag;
+		}
+		else
+		{
+			materialTypeInBag = materialTarget.MaterialType;
+			AmountOfMaterialInBag = GatherEfficiency;
+			//progressBar.Value = AmountOfMaterialInBag;
+		}
+	}
 }
